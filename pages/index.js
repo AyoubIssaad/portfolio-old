ef46a11bdc47ed5e4c8d1ef101e3ffa8f4c48e6f
ayoubissaad <ayoub.issaad@gmail.com>
2021-12-24 12:16:30 +0100

    Folder Architecture and initial dev

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import { Container, Box } from "@chakra-ui/react";
+
+const Page = () => {
+  return (
+    <Container>
+      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+        Hello, I&apos;m a full-stack, network and systems administrator based in
+        Japan!
+      </Box>
+    </Container>
+  );
+};
+
+export default Page;

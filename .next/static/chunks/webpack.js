622fc4cec389226b0a01c52449e605a831a78094
ayoubissaad <ayoub.issaad@gmail.com>
2022-01-12 12:04:32 +0100

    fonts

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e9a330ce7b9c3b00"; }
+/******/ 		__webpack_require__.h = function() { return "7364d769dd46e0e7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

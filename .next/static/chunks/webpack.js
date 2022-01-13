211431de55bca0d6bee2e7d40922b146ed8b9bd4
Ayoub Issaad <ayoub.issaad@gmail.com>
2022-01-13 23:12:29 +0100

    Fixed error

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0897547cc74b1e2c"; }
+/******/ 		__webpack_require__.h = function() { return "6edde9def781ab7f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
